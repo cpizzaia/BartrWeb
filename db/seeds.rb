58aa79bb92be1801fe040ef807d8c8f8fa370b67
Cody Pizzaia <cpizzaia@asu.edu>
2016-01-29 16:04:39 -0500

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: "bartr", password: "123456", location: "somewhere")
